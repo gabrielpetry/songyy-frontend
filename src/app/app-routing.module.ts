eecd5738377295838d8c8f6a749bc08f1442b93d
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-03 21:16:38 -0300

    Fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   {
     path: "playlist/:id",
     component: PlaylistManagerComponent,
-    canActivate: [AuthGuard, AuthService]
+    canActivate: [AuthGuard]
   }
 ];
 
