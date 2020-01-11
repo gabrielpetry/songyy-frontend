eb90e5532aa878e2de5141bf7dd3ca86ccb62dd6
Gabriel Petry <me@gabrielpetry.com.br>
2020-01-11 18:38:41 -0300

    Added logout route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { PlaylistsComponent } from "./guards/playlists/playlists.component";
 import { SongManagerComponent } from "./guards/song-manager/song-manager.component";
+import { LogoutComponent } from "./guards/logout/logout.component";
 import { LoginComponent } from "./login/login.component";
 import { AuthGuard } from "./guards/auth.guard";
 import { AuthService } from "./login/auth.service";
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "register", component: RegistrationComponent },
   { path: "playlist", component: PlaylistsComponent, canActivate: [AuthGuard] },
+  { path: "logout", component: LogoutComponent, canActivate: [AuthGuard] },
   {
     path: "playlist/:playlistName/:guildId",
     component: SongManagerComponent,
